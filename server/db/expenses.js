7c95df6aef0046e58b671a316f62e5f4c2595752
Abdi Abdilahi <111522727+abdi-abdilahi@users.noreply.github.com>
2022-12-04 13:46:05 +1300

    Add expenses db functions

--- server/db/expenses.js
+++ server/db/expenses.js
@@ -1,1 +1,26 @@
 const connection = require('./connection')
+
+module.exports = {
+  getExpenses,
+  addExpense,
+  updateExpense,
+  deleteExpense,
+}
+
+function getExpenses(budgetId, db = connection) {
+  return db('Expenses')
+    .where('budget_id', budgetId)
+    .select('id', 'name', 'amount')
+}
+
+function addExpense(newExpense, db = connection) {
+  return db('Expenses').insert(newExpense, ['id', 'name', 'amount'])
+}
+
+function updateExpense(id, expense, db = connection) {
+  return db('Expenses').where({ id }).update(expense, ['id', 'name', 'amount'])
+}
+
+function deleteExpense(id, db = connection) {
+  return db('Expenses').where({ id }).del()
+}
