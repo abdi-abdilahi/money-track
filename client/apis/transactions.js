36d9d210facf34bb49c4d0f0ffa90491ad6f6ae2
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2022-12-04 23:26:19 +1300

    Add transactions api client

--- client/apis/transactions.js
+++ client/apis/transactions.js
@@ -1,2 +1,40 @@
 import request from 'superagent'
 const rootUrl = '/api/v1/transactions/'
+
+export function getTransactions() {
+  return request
+    .get(rootUrl)
+    .then((res) => res.body)
+    .catch((err) => {
+      return { message: err.message }
+    })
+}
+
+export function addTransaction(newTransaction) {
+  return request
+    .post(rootUrl)
+    .send(newTransaction)
+    .then((res) => res.body)
+    .catch((err) => {
+      return { message: err.message }
+    })
+}
+
+export function deleteTransaction(transactionId) {
+  return request
+    .del(rootUrl + transactionId)
+    .then(() => {})
+    .catch((err) => {
+      return { message: err.message }
+    })
+}
+
+export function updateTransaction(transactionId, updatedTransaction) {
+  return request
+    .patch(rootUrl + transactionId)
+    .send(updatedTransaction)
+    .then((res) => res.body)
+    .catch((err) => {
+      return { message: err.message }
+    })
+}
