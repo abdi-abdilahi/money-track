b4deff4c33a696d8500682b48d207e5cce99bc9d
Mandeep <93898798+mandeep-sethi@users.noreply.github.com>
2022-12-04 23:29:21 +1300

    Add `<TransactionsList />` component

--- client/components/Transactions/TransactionsList.jsx
+++ client/components/Transactions/TransactionsList.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchTransactions } from '../../actions/transactions'
+import { fetchExpenses } from '../../actions/expenses'
 
-function TransactionsList() {
-  return <div>TransactionsList</div>
+import Transaction from './Transaction'
+import TransactionForm from './TransactionForm'
+
+export default function TransactionsList() {
+  const transactions = useSelector((state) => state.transactions)
+  const expenses = useSelector((state) => state.expenses)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchExpenses(1))
+    dispatch(fetchTransactions())
+  }, [])
+
+  return (
+    <div>
+      <div>
+        {transactions.loading && expenses.loading && <p>Loading.....</p>}
+      </div>
+      <div>
+        {transactions.error && expenses.error && <p>Something went wrong!</p>}
+      </div>
+      {expenses.data && <TransactionForm expenses={expenses} />}
+
+      <ul>
+        {transactions.data?.map((transaction, i) => {
+          return (
+            <Transaction
+              key={i}
+              transaction={transaction}
+              expenses={expenses}
+            />
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
-
-export default TransactionsList
