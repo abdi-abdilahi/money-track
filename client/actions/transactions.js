bf2b402952a143e6d6d76929028d0b3f8901c204
Mandeep <93898798+mandeep-sethi@users.noreply.github.com>
2022-12-04 23:27:29 +1300

    Add thunks actions

--- client/actions/transactions.js
+++ client/actions/transactions.js
@@ -1,6 +1,108 @@
+import {
+  getTransactions,
+  addTransaction,
+  deleteTransaction,
+  updateTransaction,
+} from '../apis/transactions'
+
 export const TRANSACTIONS_PENDING = 'TRANSACTIONS_PENDING'
 export const TRANSACTIONS_REJECTED = 'TRANSACTIONS_REJECTED'
 export const GET_TRANSACTIONS_FULFILLED = 'GET_TRANSACTIONS_FULFILLED'
 export const ADD_TRANSACTION_FULFILLED = 'ADD_TRANSACTION_FULFILLED'
 export const UPDATE_TRANSACTION_FULFILLED = 'UPDATE_TRANSACTION_FULFILLED'
 export const DELETE_TRANSACTION_FULFILLED = 'DELETE_TRANSACTION_FULFILLED'
+
+function transactionsPending() {
+  return {
+    type: TRANSACTIONS_PENDING,
+  }
+}
+
+function transactionsRejected(errorMessage) {
+  return {
+    type: TRANSACTIONS_REJECTED,
+    payload: errorMessage,
+  }
+}
+
+function getTransactionsFulfilled(transactions) {
+  return {
+    type: GET_TRANSACTIONS_FULFILLED,
+    payload: transactions,
+  }
+}
+
+function addTransactionFulfilled(newTransaction) {
+  return {
+    type: ADD_TRANSACTION_FULFILLED,
+    payload: newTransaction,
+  }
+}
+
+function updateTransactionFulfilled(oldTransactionId, newTransaction) {
+  return {
+    type: UPDATE_TRANSACTION_FULFILLED,
+    payload: { oldTransactionId, newTransaction },
+  }
+}
+
+function deleteTransactionFulfilled(id) {
+  return {
+    type: DELETE_TRANSACTION_FULFILLED,
+    payload: id,
+  }
+}
+
+export function fetchTransactions() {
+  return (dispatch) => {
+    dispatch(transactionsPending())
+    return getTransactions()
+      .then((transactions) => {
+        dispatch(getTransactionsFulfilled(transactions))
+      })
+      .catch((err) => {
+        dispatch(transactionsRejected(err.message))
+      })
+  }
+}
+
+export function postTransaction(newTransaction) {
+  return (dispatch) => {
+    dispatch(transactionsPending())
+    return addTransaction(newTransaction)
+      .then((transaction) => {
+        dispatch(addTransactionFulfilled(transaction))
+      })
+      .catch((err) => {
+        dispatch(transactionsRejected(err.message))
+      })
+  }
+}
+
+export function patchTransaction(oldTransactionId, newTransaction) {
+  return (dispatch) => {
+    dispatch(transactionsPending())
+    return updateTransaction(oldTransactionId, newTransaction)
+      .then((updatedTransaction) => {
+        dispatch(
+          updateTransactionFulfilled(oldTransactionId, updatedTransaction)
+        )
+      })
+      .catch((err) => {
+        dispatch(transactionsRejected(err.message))
+      })
+  }
+}
+
+export function delTransaction(transactionId) {
+  return (dispatch) => {
+    dispatch(transactionsPending())
+    return deleteTransaction(transactionId)
+      .then(() => {
+        dispatch(deleteTransactionFulfilled(transactionId))
+      })
+      .catch((err) => {
+        dispatch(transactionsRejected(err.message))
+      })
+  }
+}
