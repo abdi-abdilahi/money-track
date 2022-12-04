55588845fe18c039cafde938add1c6afa6b1e3e6
Abdi Abdilahi <111522727+abdi-abdilahi@users.noreply.github.com>
2022-12-04 14:50:02 +1300

    Add expenses API client

--- client/apis/expenses.js
+++ client/apis/expenses.js
@@ -1,2 +1,40 @@
 import request from 'superagent'
 const rootUrl = '/api/v1/expenses/'
+
+export function getExpenses(budgetId) {
+  return request
+    .get(rootUrl + budgetId)
+    .then((res) => res.body)
+    .catch((err) => {
+      return { message: err.message }
+    })
+}
+
+export function addExpense(budgetId, expenses) {
+  return request
+    .post(rootUrl + budgetId)
+    .send(expenses)
+    .then((res) => res.body)
+    .catch((err) => {
+      return { message: err.message }
+    })
+}
+
+export function updateExpense(expenseId, newExpense) {
+  return request
+    .patch(rootUrl + expenseId)
+    .send(newExpense)
+    .then((res) => res.body)
+    .catch((err) => {
+      return { message: err.message }
+    })
+}
+
+export function deleteExpense(expenseId) {
+  return request
+    .delete(rootUrl + expenseId)
+    .then(() => {})
+    .catch((err) => {
+      return { message: err.message }
+    })
+}
