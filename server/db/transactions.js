eec6aeea38c3e51f182131c24ec610105897dd9b
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2022-12-04 20:33:12 +1300

    Add transactions db functions

--- server/db/transactions.js
+++ server/db/transactions.js
@@ -1,1 +1,44 @@
 const connection = require('./connection')
+
+module.exports = {
+  getTransactions,
+  addTransaction,
+  updateTransaction,
+  deleteTransaction,
+}
+
+function getTransactions(db = connection) {
+  return db('Transactions').select(
+    'id',
+    'name',
+    'amount',
+    'date_created as dateCreated',
+    'expenses_id as expensesId'
+  )
+}
+
+function addTransaction(newTransaction, db = connection) {
+  return db('Transactions').insert(newTransaction, [
+    'id',
+    'name',
+    'amount',
+    'date_created as dateCreated',
+    'expenses_id as expensesId',
+  ])
+}
+
+function updateTransaction(id, updatedTransaction, db = connection) {
+  return db('Transactions')
+    .where({ id })
+    .update(updatedTransaction, [
+      'id',
+      'name',
+      'amount',
+      'date_created as dateCreated',
+      'expenses_id as expensesId',
+    ])
+}
+
+function deleteTransaction(id, db = connection) {
+  return db('Transactions').where({ id }).del()
+}
