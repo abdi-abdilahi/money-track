d0b5b4d0dff2a590a272994bd1c4729b3e8a7797
Mandeep <93898798+mandeep-sethi@users.noreply.github.com>
2022-12-05 16:04:18 +0800

    Another layout option - partial

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -8,10 +8,12 @@ function Layout() {
   return (
     <Box sx={{ height: '100vh' }}>
       <Grid container>
-        <Grid item xs={4}>
+        {/* <Grid item sx={{ width: '20vw' }}> */}
+        <Grid item xs={3}>
           <Sidebar />
         </Grid>
-        <Grid item xs={12} sm={6}>
+        {/* <Grid item sx={{ width: '70vw'}} > */}
+        <Grid item xs={12} sm={8} md={8} lg={8}>
           <Outlet />
         </Grid>
       </Grid>
