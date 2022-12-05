551cfba3a8ba6a576487cd85f3db4fcc4115a3b9
William Santos <62542231+williamjgsantos@users.noreply.github.com>
2022-12-05 13:02:10 +1300

    Update budget table. Add budget name

--- server/db/seeds/budget.js
+++ server/db/seeds/budget.js
@@ -5,6 +5,7 @@ exports.seed = async function (knex) {
     {
       id: 1,
       user_id: '2',
+      name: 'Personal',
       start_date: new Date('2022-12-04T00:00:01'),
       end_date: new Date('2022-12-10T00:00:01'),
     },
