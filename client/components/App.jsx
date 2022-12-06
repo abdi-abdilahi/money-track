d119034dafbba4a74ba556c1cd45d64103f22329
William Santos <62542231+williamjgsantos@users.noreply.github.com>
2022-12-07 12:48:00 +1300

    alowiing the user to log in/out using Auth0

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-
+import { useAuth0 } from '@auth0/auth0-react'
 import Layout from './Layout'
 import Dashboard from './Dashboard/Dashboard'
 import TransactionsList from './Transactions/TransactionsList'
@@ -8,14 +8,26 @@ import NotFound from './NotFound'
 import Budgets from './Budgets/Budgets'
 
 export default function App() {
+  const { loginWithRedirect, isAuthenticated } = useAuth0()
+
+  const handleSignIn = (e) => {
+    e.preventDefault()
+    loginWithRedirect()
+  }
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="/transactions" element={<TransactionsList />} />
-        <Route path="/expenses/:budgetId" element={<Budgets />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <>
+      {isAuthenticated ? (
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/transactions" element={<TransactionsList />} />
+            <Route path="/expenses/:budgetId" element={<Budgets />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      ) : (
+        <button onClick={handleSignIn}>Log In</button>
+      )}
+    </>
   )
 }
--- client/components/Sidebar/Sidebar.jsx
+++ client/components/Sidebar/Sidebar.jsx
@@ -57,7 +57,7 @@ function Sidebar() {
             />
           </List>
 
-          <UserInfo name={'John Doe'} />
+          <UserInfo />
         </Grid>
       </Paper>
     </Grid>
