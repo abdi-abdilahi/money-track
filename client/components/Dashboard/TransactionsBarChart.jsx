017aa50b699850fdd5de0860de755f1f38fc0d0b
Mandeep <93898798+mandeep-sethi@users.noreply.github.com>
2022-12-06 14:56:16 +0800

    Bar chart completed

--- client/components/Dashboard/TransactionsBarChart.jsx
+++ client/components/Dashboard/TransactionsBarChart.jsx
@@ -1,71 +1,47 @@
-import React from 'react'
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from 'recharts'
-
-const data = [
-  {
-    name: 'Jan',
-    'Total Spent': 4000,
-  },
-  {
-    name: 'Feb',
-    'Total Spent': 3000,
-  },
-  {
-    name: 'Mar',
-    'Total Spent': 2000,
-  },
-  {
-    name: 'Apr',
-    'Total Spent': 2800,
-  },
-  {
-    name: 'May',
-    'Total Spent': 3500,
-  },
-  {
-    name: 'Jun',
-    'Total Spent': 4500,
-  },
-  {
-    name: 'Jul',
-    'Total Spent': 4300,
-  },
-  {
-    name: 'Aug',
-    'Total Spent': 3908,
-  },
-  {
-    name: 'Sep',
-    'Total Spent': 4800,
-  },
-  {
-    name: 'Oct',
-    'Total Spent': 3800,
-  },
-  {
-    name: 'Nov',
-    'Total Spent': 4300,
-  },
-  {
-    name: 'Dec',
-    'Total Spent': 4300,
-  },
-]
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchTransactions } from '../../actions/transactions'
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 
 export default function App() {
+  const transactionsData = useSelector((state) => state.transactions)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchTransactions())
+  }, [])
+
+  const transactionHistory = [
+    { month: 'Jan', total: 0 },
+    { month: 'Feb', total: 0 },
+    { month: 'Mar', total: 0 },
+    { month: 'Apr', total: 0 },
+    { month: 'May', total: 0 },
+    { month: 'Jun', total: 0 },
+    { month: 'Jul', total: 0 },
+    { month: 'Aug', total: 0 },
+    { month: 'Sep', total: 0 },
+    { month: 'Oct', total: 0 },
+    { month: 'Nov', total: 0 },
+    { month: 'Dec', total: 0 },
+  ]
+
+  const sortData =
+    transactionsData.data?.sort(
+      (a, b) => new Date(b.dateCreated) - new Date(a.dateCreated)
+    ) || null
+
+  sortData?.map((transaction) => {
+    const date = transaction.dateCreated.split(/[- :]/)
+    const monthHistory = transactionHistory[date[1] - 1]
+    monthHistory.total += transaction.amount
+  })
+
   return (
     <BarChart
       width={600}
       height={300}
-      data={data}
+      data={transactionHistory}
       margin={{
         top: 5,
         right: 30,
@@ -74,12 +50,11 @@ export default function App() {
       }}
       barSize={20}
     >
-      <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+      <XAxis dataKey="month" scale="point" padding={{ left: 10, right: 10 }} />
       <YAxis />
       <Tooltip />
       <Legend />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Bar dataKey="Total Spent" fill="#0F3D3E" background={{ fill: '#eee' }} />
+      <Bar dataKey="total" fill="#0F3D3E" />
     </BarChart>
   )
 }
